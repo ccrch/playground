d1b98925558bb2111f9a18082db056b79ea70dc2
Mike <mike@vettvangur.is>
2025-07-03 23:12:23 +0000

    different pinning approach

---
+++ src/components/text.ts
@@ -0,0 +1,49 @@
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+const pinText = () => {
+  gsap.registerPlugin(ScrollTrigger)
+  
+  const q = gsap.utils.selector('.scrolltriggers-test')
+  
+  q('.title').forEach((section) => {
+    gsap.set(section, { opacity: 0 })
+  
+    ScrollTrigger.create({
+      onEnter: () => {
+        gsap.to(section, { opacity: 1 })
+      },
+      onLeaveBack: () => {
+        gsap.to(section, { opacity: 0 })
+      },
+      start: () => '0% 90%',
+      trigger: section.querySelector('div'),
+    })
+  
+    ScrollTrigger.create({
+      end: () => '+=100%',
+      // pin: section.querySelector('div'),
+      pin: true,
+      pinSpacing: false,
+      start: () => '50% 50%',
+      // trigger: section,
+      trigger: section.querySelector('div'),
+    })
+  })
+  
+  q('.text').forEach((section) => {
+    ScrollTrigger.create({
+      onEnter: () => {
+        gsap.to(section.previousElementSibling, { opacity: 0 })
+      },
+      onLeaveBack: () => {
+        gsap.to(section.previousElementSibling, { opacity: 1 })
+      },
+      start: '0% 50%',
+      trigger: section,
+    })
+  })
+}
+
+export default pinText
+
