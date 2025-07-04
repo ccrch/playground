75b6f2379060c2a24d197353f58a0821751fc9a4
Mike <mike@vettvangur.is>
2025-07-04 16:15:48 +0000

    isTouch condition fix

--- src/components/text.ts
+++ src/components/text.ts
@@ -29,7 +29,7 @@ const pinText = () => {
       const { isMobile } = context.conditions
 
       ScrollTrigger.create({
-        anticipatePin: isMobile ? (ScrollTrigger.isTouch ? 0.7 : 0) : 0,
+        anticipatePin: isMobile ? (ScrollTrigger.isTouch === 1 ? 0.7 : 0) : 0,
         end: '+=100%',
         // pin: section.querySelector('div'),
         pin: true,
