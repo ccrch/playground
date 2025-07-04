04a40686d157d75b2a23b4f8a485abbe9c2a68bc
Mike <mike@vettvangur.is>
2025-07-04 12:23:50 +0000

    astro helpers + paths fix

---
+++ src/scripts/core/astro-helpers.ts
@@ -0,0 +1,43 @@
+
+export const randomText = (min = 10, max = 30) => {
+  const amount = Math.floor(Math.random() * (max - min + 1)) + min // gsap.utils.random(min, max)
+  let text = ''
+  const words = [
+    'lorem',
+    'ipsum',
+    'dolor',
+    'sit',
+    'amet',
+    'consectetur',
+    'adipiscing',
+    'elit',
+    'sed',
+    'do',
+    'eiusmod',
+    'tempor',
+    'incididunt',
+    'ut',
+    'labore',
+    'et',
+    'dolore',
+    'magna',
+    'aliqua',
+  ]
+
+  Array.from({ length: amount }).forEach(() => {
+    const randomIndex = Math.floor(Math.random() * words.length)
+    text += `${words[randomIndex]} `
+  })
+
+  text = text.trim()
+  text = `${text.charAt(0).toUpperCase()}${text.slice(1)}.`
+
+  return text
+}
+
+export const url = (url) => {
+  const isDev = process.env.NODE_ENV === 'development'
+  const base = isDev ? '/astro-test' : 'https://ccrch.github.io/astro-test'
+
+  return `${base}${url}`
+}
--- src/scripts/core/helpers.ts
+++ src/scripts/core/helpers.ts
@@ -148,39 +148,3 @@ export const verticalResizeCheck = {
     })
   },
 }
-
-export const randomText = (min = 10, max = 30) => {
-  const amount = Math.floor(Math.random() * (max - min + 1)) + min // gsap.utils.random(min, max)
-  let text = ''
-  const words = [
-    'lorem',
-    'ipsum',
-    'dolor',
-    'sit',
-    'amet',
-    'consectetur',
-    'adipiscing',
-    'elit',
-    'sed',
-    'do',
-    'eiusmod',
-    'tempor',
-    'incididunt',
-    'ut',
-    'labore',
-    'et',
-    'dolore',
-    'magna',
-    'aliqua',
-  ]
-
-  Array.from({ length: amount }).forEach(() => {
-    const randomIndex = Math.floor(Math.random() * words.length)
-    text += `${words[randomIndex]} `
-  })
-
-  text = text.trim()
-  text = `${text.charAt(0).toUpperCase()}${text.slice(1)}.`
-
-  return text
-}
