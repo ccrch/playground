60ed4a226b3af6999e867fe25473416fa286ba9e
Mike <mike@vettvangur.is>
2025-07-25 10:33:22 +0000

    improved section headline reveal

--- src/scripts/playground/branding-page.ts
+++ src/scripts/playground/branding-page.ts
@@ -101,14 +101,17 @@ const brandingPage = {
         const el = {
           content: section.querySelector('.section__content'),
           headline: section.querySelector('.section__headline'),
-          headlineLines: section.querySelectorAll('.section__headline-eyebrow .split-line, .section__headline h1 .split-line'),
+          headlineChars: section.querySelectorAll('.section__headline h1 .split-char'),
+          headlineEyebrow: section.querySelectorAll('.section__headline p .split-line'),
+          headlineLines: section.querySelectorAll('.section__headline h1 .split-line'),
         }
   
         // Reset headlines - on scroll down & scroll back up
   
         ScrollTrigger.create({
           onEnter: () => {
-            gsap.set(el.headlineLines, { overwrite: true, y: '100%' })
+            gsap.set([el.headlineEyebrow, el.headlineChars], { overwrite: true, y: '100%' })
+            gsap.set(el.headlineLines, { overwrite: true, y: '0%' })
           },
           start: '0% 100%',
           trigger: section,
@@ -116,7 +119,8 @@ const brandingPage = {
   
         ScrollTrigger.create({
           onEnter: () => {
-            gsap.set(el.headlineLines, { overwrite: true, y: '100%' })
+            gsap.set([el.headlineEyebrow, el.headlineChars], { overwrite: true, y: '100%' })
+            gsap.set(el.headlineLines, { overwrite: true, y: '0%' })
           },
           start: '100% 0%',
           trigger: section,
@@ -126,11 +130,12 @@ const brandingPage = {
   
         ScrollTrigger.create({
           onEnter: () => {
-            gsap.to(el.headlineLines, {
+            gsap.set(el.headlineLines, { overwrite: true, y: '0%' })
+            gsap.to([el.headlineEyebrow, el.headlineChars], {
               duration: 1.234,
               ease: 'power3.out',
               opacity: 1,
-              stagger: 0.0876,
+              stagger: 0.021,
               y: '0%',
             })
           },
@@ -142,7 +147,8 @@ const brandingPage = {
   
         ScrollTrigger.create({
           onEnter: () => {
-            gsap.to(el.headlineLines, {
+            // gsap.set(el.headlineChars, { overwrite: true, y: '0%' })
+            gsap.to([el.headlineEyebrow, el.headlineLines], {
               duration: 0.876,
               ease: 'power3.inOut',
               overwrite: true,
@@ -151,13 +157,14 @@ const brandingPage = {
             })
           },
           onLeaveBack: () => {
-            gsap.fromTo(el.headlineLines, {
+            gsap.set(el.headlineLines, { overwrite: true, y: '0%' })
+            gsap.fromTo([el.headlineEyebrow, el.headlineChars], {
               y: '100%',
             }, {
               duration: 1.234,
               ease: 'power3.out',
               opacity: 1,
-              stagger: 0.0876,
+              stagger: 0.021,
               y: '0%',
             })
           },
@@ -185,13 +192,21 @@ const brandingPage = {
     splitText(): void {
       // Split the text & reset it
 
-      SplitText.create('.section:not(.section--home) .section__headline h1, .section:not(.section--home) .section__headline p', {
+      SplitText.create('.section:not(.section--home) .section__headline p', {
         linesClass: 'split-line',
         mask: 'lines',
         type: 'lines',
       })
 
-      gsap.set('.section:not(.section--home) .section__headline .split-line', { overwrite: true, y: '100%' })
+      SplitText.create('.section:not(.section--home) .section__headline h1', {
+        charsClass: 'split-char',
+        linesClass: 'split-line',
+        mask: 'lines',
+        type: 'chars, lines',
+      })
+
+      gsap.set('.section:not(.section--home) .section__headline p .split-line', { overwrite: true, y: '100%' })
+      gsap.set('.section:not(.section--home) .section__headline h1 .split-char', { overwrite: true, y: '100%' })
     },
   },
 
